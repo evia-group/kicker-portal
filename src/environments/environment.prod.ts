2993a7b4e8acd9adc0a6b1bd354f14acf646712c
dirk.mueller@evia.de <p7vlvvnnfq7g2cnen44zw3pdfqjmm46chodgvcs4tfo647qyrlcq>
2020-10-13 18:11:56 +0200

    fix: change prefix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   versionNumber: '#{versionNumber}#',
-  prefix: '#{prefix}#',
+  prefix: '',
   firebase: {
     apiKey: '#{firebaseApiKey}#',
     authDomain: '#{firebaseAuthDomain}#',
