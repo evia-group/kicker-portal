98c0087000a4e7fdb1b1c1ac98c4f26bcac5d46c
Dirk Müller <dirk.mueller@evia.de>
2022-06-14 23:03:40 +0200

    chore(environment): change workflow

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,24 +2,4 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: true,
-  versionNumber: '#{versionNumber}#',
-  prefix: '',
-  firebase: {
-    apiKey: '#{firebaseApiKey}#',
-    authDomain: '#{firebaseAuthDomain}#',
-    databaseURL: '#{firebaseDatabaseURL}#',
-    projectId: '#{firebaseProjectId}#',
-    storageBucket: '#{firebaseStorageBucket}#',
-    messagingSenderId: '#{firebaseMessagingSenderId}#',
-    appId: '#{firebaseAppId}#',
-  },
-  globals: {
-    team1: 0,
-    team2: 1,
-    round1: 0,
-    round2: 1,
-    round3: 2,
-  }
-};
+export const environment = {};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,27 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  versionNumber: 'v0.0.1',
-  prefix: 'T-',
-  firebase: {
-    apiKey: 'AIzaSyBbBYhVMNcLUm3PRatlB2IPACxO_VamlkA',
-    authDomain: 'evia-desk.firebaseapp.com',
-    databaseURL: 'https://evia-desk.firebaseio.com',
-    projectId: 'evia-desk',
-    storageBucket: 'evia-desk.appspot.com',
-    messagingSenderId: '124473984293',
-    appId: '1:124473984293:web:578828e4a4140cad2fc6cc',
-  },
-  globals: {
-    team1: 0,
-    team2: 1,
-    round1: 0,
-    round2: 1,
-    round3: 2,
-  }
-};
+export const environment = {};
 
 /*
  * For easier debugging in development mode, you can import the following file
