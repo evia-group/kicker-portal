4c805866626427e903ae0205d328e337d175865e
Dirk Müller <dirk.mueller@evia.de>
2022-06-15 18:51:34 +0200

    chore(login): clean up env ci

--- env.ci.js
+++ env.ci.js
@@ -1,55 +1,16 @@
-// const fs = require('fs');
-// const path = require('path');
-// const {version} = require('./package.json');
-//
-// const dir = "src/environments";
-// const file = "environment.ts";
-// const prodFile = "environment.prod.ts";
-//
-// const content = `${process.env.FIREBASE_DETAILS}`;
-//
-// fs.access(dir, fs.constants.F_OK, (err) => {
-//   if(err) {
-//     // Directory doesn't exist
-//     console.log('src doesn\'t exist, creating now', process.cwd());
-//     // Create /src
-//     fs.mkdir(dir, {recursive: true}, (err) => {
-//       if (err) throw err;
-//     })
-//   }
-//   // Now write to file
-//   try {
-//     fs.writeFileSync(dir + "/" + file, content);
-//     fs.writeFileSync(dir + "/" + prodFile, content);
-//     console.log('Created successfully in', process.cwd());
-//     if (fs.existsSync(dir + "/" + file)) {
-//       console.log('File is created', path.resolve(dir + "/" + file));
-//       const str = fs.readFileSync(dir + "/" + file).toString();
-//       console.log(str);
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     process.exit(1);
-//   }
-// })
-
-
-
-/* tslint:disable */
-// @ts-nocheck
 const { writeFile, existsSync, mkdirSync } = require('fs');
 const {version} = require('./package.json');
 
 require('dotenv').config();
 
 
-function writeFileUsingFS(targetPath, environmentFileContent) {
-  writeFile(targetPath, environmentFileContent, function (err) {
+function writeFileUsingFS(targetPathFS, environmentFileContentFS) {
+  writeFile(targetPathFS, environmentFileContentFS, function (err) {
     if (err) {
       console.log(err);
     }
-    if (environmentFileContent !== '') {
-      console.log(`wrote variables to ${targetPath}`);
+    if (environmentFileContentFS !== '') {
+      console.log(`wrote variables to ${targetPathFS}`);
     }
   });
 }
@@ -101,5 +62,3 @@ const environmentFileContent = `
 `;
 
 writeFileUsingFS(targetPath, environmentFileContent); // appending data into the target file
-
-/* tslint:enable */
