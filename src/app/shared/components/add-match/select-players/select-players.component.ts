40708be162d3a003b63abb73e46d95ef836c7871
Komail Mohammadi <komail.mohammadi@evia.de>
2022-07-01 17:20:33 +0200

    fix: user can be selected only once

--- src/app/shared/components/add-match/select-players/select-players.component.ts
+++ src/app/shared/components/add-match/select-players/select-players.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { ITeam, IUser } from '../../../interfaces/user.interface';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { FormGroup } from '@angular/forms';
+import { UsersService } from 'src/app/shared/services/users.service';
 
 @Component({
   selector: 'app-select-players',
@@ -18,5 +19,42 @@ export class SelectPlayersComponent {
   @Input()
   matchForm: FormGroup;
 
+  playersList: IUser[] = [];
+
+  allPlayers: IUser[] = [];
+
+  constructor(userService: UsersService) {
+    userService.users$.subscribe(data => {
+      data.forEach((player: IUser) => {
+        let isNew = true;
+        this.allPlayers.forEach(player2 => {
+          if (player.id === player2.id) {
+            isNew = false;
+          }
+        });
+        if (isNew) {
+          this.allPlayers.push(player);
+        }
+      });
+    });
+  }
+
   teams: number[] = [0, 1];
+
+  selectedPlayers: string[] = [];
+
+  updatePlayers(event: any) {
+    this.selectedPlayers[+event.source.id] = event.value.id;
+  }
+
+  createPlayersList(selId: string) {
+    console.log(this.selectedPlayers);
+    let selList: IUser[] = [];
+    this.allPlayers.forEach(player => {
+      if (!this.selectedPlayers.includes(player.id) || this.selectedPlayers.indexOf(player.id) === +selId) {
+        selList.push(player);
+      }
+    })
+    return selList;
+  }
 }
