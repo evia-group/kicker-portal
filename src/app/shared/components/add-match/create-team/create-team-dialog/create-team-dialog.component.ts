a13724c9700cbb57f26ffb810a88591b5bf7f953
Komail Mohammadi <komail.mohammadi@evia.de>
2022-07-07 15:58:41 +0200

    feat: create team with button
    remove unused imports

--- src/app/shared/components/add-match/create-team/create-team-dialog/create-team-dialog.component.ts
+++ src/app/shared/components/add-match/create-team/create-team-dialog/create-team-dialog.component.ts
@@ -1,5 +1,5 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 import { UsersService } from 'src/app/shared/services/users.service';
 
 export interface DialogData {
