35e5169cc0cb530447ddce62e32c61e90afd5479
Komail Mohammadi <komail.mohammadi@evia.de>
2023-06-05 12:17:01 +0200

    feat: showing button instead of selections when screen to small

--- src/app/shared/components/add-match/select-players/two-selections/two-selections.component.ts
+++ src/app/shared/components/add-match/select-players/two-selections/two-selections.component.ts
@@ -1,14 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-two-selections',
   templateUrl: './two-selections.component.html',
   styleUrls: ['./two-selections.component.scss'],
 })
-export class TwoSelectionsComponent implements OnInit {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  constructor() {}
+export class TwoSelectionsComponent implements OnInit, OnDestroy {
+  @Input()
+  formGroupName;
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  ngOnInit(): void {}
+  @Input()
+  firstPlayerControl: FormControl;
+
+  @Input()
+  secondPlayerControl: FormControl;
+
+  @Input()
+  firstPlayerOptions;
+
+  @Input()
+  secondPlayerOptions;
+
+  @Input()
+  displayWithFunction;
+
+  @Input()
+  placeholderText: string;
+
+  @Input()
+  labelText: string;
+
+  breakpointObserverSubscription: Subscription;
+
+  showButton = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.breakpointObserverSubscription = this.breakpointObserver
+      .observe(Breakpoints.XSmall)
+      .subscribe((result) => {
+        this.showButton = result.matches;
+        console.log(result);
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.breakpointObserverSubscription) {
+      this.breakpointObserverSubscription.unsubscribe();
+    }
+  }
 }
