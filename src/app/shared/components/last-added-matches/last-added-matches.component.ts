2e2db5459e3aa6a7b09625c52c6c03cf721491e6
Komail Mohammadi <komail.mohammadi@evia.de>
2023-07-31 10:12:23 +0200

    feat: added view for last matches

--- src/app/shared/components/last-added-matches/last-added-matches.component.ts
+++ src/app/shared/components/last-added-matches/last-added-matches.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatchesService } from '../../services/matches.service';
+import { IMatch } from '../../interfaces/match.interface';
+import { TeamsService } from '../../services/teams.service';
+import { combineLatestWith } from 'rxjs';
+import { ITeam } from '../../interfaces/user.interface';
 
 @Component({
   selector: 'app-last-added-matches',
@@ -6,9 +11,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./last-added-matches.component.scss'],
 })
 export class LastAddedMatchesComponent implements OnInit {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  constructor() {}
+  numberOfMatchesToShow = 5;
+  lastAddedMatches = [];
+  constructor(
+    private matchesService: MatchesService,
+    private teamsService: TeamsService
+  ) {}
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.matchesService.matchesSub$
+      .pipe(combineLatestWith(this.teamsService.teams$))
+      .subscribe((data: [IMatch[], ITeam[]]) => {
+        this.lastAddedMatches.length = 0;
+        const matches = data[0];
+        const teams = data[1];
+        if (this.numberOfMatchesToShow > matches.length) {
+          this.numberOfMatchesToShow = matches.length;
+        }
+        matches.sort((a, b) => {
+          return b.date.toMillis() - a.date.toMillis();
+        });
+        // console.log(data);
+        const lastMatches = matches.slice(0, this.numberOfMatchesToShow);
+        // console.log(lastMatches);
+        lastMatches.forEach((match) => {
+          const teamIds = Object.keys(match.result);
+          const resultTeam1 = match.result[teamIds[0]];
+          const resultTeam2 = match.result[teamIds[1]];
+          const nameTeam1 = this.getTeamNameById(teamIds[0], teams);
+          const nameTeam2 = this.getTeamNameById(teamIds[1], teams);
+          if (nameTeam1 && nameTeam2) {
+            this.lastAddedMatches.push({
+              team1: nameTeam1,
+              team2: nameTeam2,
+              result: resultTeam1 + ':' + resultTeam2,
+            });
+          }
+        });
+      });
+  }
+
+  getTeamNameById(teamId: string, teams: ITeam[]) {
+    const foundTeam = teams.find((team) => team.id === teamId);
+    return foundTeam ? foundTeam.name : undefined;
+  }
 }
